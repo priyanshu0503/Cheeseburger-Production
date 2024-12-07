59d160623e003b8a9b7f980cc4a0b30a84ff8659
priyanshu0503 <priyanshu625@yahoo.com>
2024-12-07 16:32:37 -0600

    Update producers.h

--- producers.h
+++ producers.h
@@ -1,24 +1,25 @@
-#ifndef PRODUCERS_H
-#define PRODUCERS_H
+#ifndef PRODUCER_H
+#define PRODUCER_H
 
 #include <pthread.h>
 #include <semaphore.h>
 
-// Buffer sizes
-#define MILK_BUFFER_SIZE 10
+// Buffer Sizes
+#define MILK_BUFFER_SIZE 9
 #define CHEESE_BUFFER_SIZE 4
 
-// Shared buffers
+// Shared Buffers
 extern int milk_buffer[MILK_BUFFER_SIZE];
 extern int cheese_buffer[CHEESE_BUFFER_SIZE];
+extern int milk_count, cheese_count;
+extern int num_cheeseburgers;
 
-// Semaphores and mutexes
-extern sem_t milk_sem;
-extern sem_t cheese_sem;
-extern pthread_mutex_t milk_mutex;
-extern pthread_mutex_t cheese_mutex;
+// Semaphores and Mutexes
+extern sem_t empty_milk, full_milk;
+extern sem_t empty_cheese, full_cheese;
+extern pthread_mutex_t milk_mutex, cheese_mutex;
 
-// Function prototypes
+// Function Prototypes
 void* milk_producer(void* arg);
 void* cheese_producer(void* arg);
 void* cheeseburger_producer(void* arg);
